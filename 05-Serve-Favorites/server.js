428fd4161715aeb2d539047a6d345d31d7d74d92
branden langhals <58713197+Brandenlanghals@users.noreply.github.com>
2020-05-09 22:19:01 -0400

    Week 11 Activities

---
+++ 05-Serve-Favorites/server.js
@@ -0,0 +1,42 @@
+// Require dependencies
+var http = require("http");
+var fs = require("fs");
+
+// Set our port to 8080
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+
+  // Capture the url the request is made to
+  var path = req.url;
+
+  // When we visit different urls, read and respond with different files
+  switch (path) {
+    case "/food":
+    case "/movies":
+    case "/frameworks":
+      return renderHTML(path, res);
+
+    // default to rendering index.html, if none of above cases are hit
+    default:
+      return renderHTML('/index.html', res);
+  }
+}
+
+function handleRequest(req, res) {
+
+    // Here we use the fs package to read our index.html file
+    fs.readFile(__dirname + "/index.html", function(err, data) {
+      if (err) throw err;
+      // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+      // an html file.
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+      });
+}
+// Starts our server.
+server.listen(PORT, function() {
+  console.log("Server is listening on PORT:" + PORT);
+});
